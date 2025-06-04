40cf7a1cec922c9f6dfed42c8bad59f5eacbfbb4
Robin <rovan.rangi@gmail.com>
2025-06-04 16:07:19 +0530

    Minor Changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,14 @@
--- lua/plugins/treesitter.lua
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate", -- auto-run :TSUpdate after install
-  event = { "BufReadPost", "BufNewFile" }, -- lazy load on file open
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua", "python", "cpp", "c", "bash", "html", "javascript", "typescript", "vim", "vimdoc"
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      -- Add more modules if needed:
-      -- incremental_selection = {...}
-      -- textobjects = {...}
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
-
