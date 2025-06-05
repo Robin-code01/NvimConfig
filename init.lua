924788612b4ebafd12fa5ff0cf3e28f3be44a6a3
Robin <rovan.rangi@gmail.com>
2025-06-05 19:01:07 +0530

    Adjusted the width of neo-tree

--- init.lua
+++ init.lua
@@ -12,3 +12,10 @@ vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true
 
 -- Yank in visual mode into system clipboard:
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
+-- To select all using ctrl+a:
+vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
+vim.keymap.set('v', '<C-a>', 'ggVG', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-a>', 'ggVG', { noremap = true, silent = true })
+
+
